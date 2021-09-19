9737cee7695827d13a5557f23ea33f9724cf13ba
GilbertSitungkir09 <82133653+GilbertSitungkir09@users.noreply.github.com>
2021-09-19 15:19:17 +0700

    Add files via upload

---
+++ src/Components/NextDay.jsx
@@ -0,0 +1,27 @@
+import { createMemo } from "solid-js";
+
+import data from "../Misc/data";
+import styles from "./NextDay.module.css";
+
+function NextDay({ nextDay }) {
+  const next = createMemo(() => data[nextDay() - 1]);
+
+  return (
+    <div className={styles.nextDay}>
+      <h3>Mata Pelajaran Selanjutnya</h3>
+      <p>
+        Hari Belajar Selanjutnya adalah hari <b>{next().hari}</b>
+      </p>
+      <p>
+        Mapel:{" "}
+        {next()
+          .mapel.filter((x) => x !== undefined)
+          .filter((x) => x !== null)
+          .join(", ")}
+        .
+      </p>
+    </div>
+  );
+}
+
+export default NextDay;
