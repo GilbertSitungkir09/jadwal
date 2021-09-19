ebe3626cacd0d70bb363d2814a3cac940c13c7f2
GilbertSitungkir09 <82133653+GilbertSitungkir09@users.noreply.github.com>
2021-09-19 15:18:22 +0700

    Add files via upload

---
+++ src/Misc/data.js
@@ -0,0 +1,42 @@
+const jadwal = [
+  {
+    hari: "Senin",
+    mapel: [null, "MM MINAT / GEOGRAFI", "BAHASA INDONESIA", "-"],
+  },
+  {
+    hari: "Selasa",
+    mapel: ["BAHASA INGGRIS", "SEJARAH WAJIB", "LINTAS MINAT"],
+  },
+  {
+    hari: "Rabu",
+    mapel: [
+      "BIOLOGI / SOSIOLOGI",
+      "AGAMA"
+    ],
+  },
+  {
+    hari: "Kamis",
+    mapel: ["MTK WAJIB", "PJOK", "PKW"],
+  },
+  {
+    hari: "Jum'at",
+    mapel: [null, "FISIKA / SEJARAH MINAT", "SENI BUDAYA"],
+  },
+  {
+    hari: "Sabtu",
+    mapel: [null, "KIMIA / EKONOMI", "PKN"],
+  },
+];
+
+const longest = jadwal
+  .map(({ mapel }) => mapel.length)
+  .sort((a, b) => b - a)[0];
+const newArray = Array.from(new Array(longest));
+
+export default jadwal;
+export const columnRemap = newArray.map((_, idx) =>
+  jadwal.map((data) => ({
+    mapel: data.mapel[idx],
+    index: jadwal.findIndex((x) => x.hari === data.hari) + 1,
+  }))
+);
